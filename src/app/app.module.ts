b88dc668bec43961fbe1f0065bf7273286b4a48e
mehmet <mhmtranas@gmail.com>
2018-01-30 00:00:53 +0300

    Added footer and fixed sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MaterialModule} from './material.module';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
