256f017726630368278d30d70277f17feda45fca
mehmet <mhmtranas@gmail.com>
2018-02-27 14:14:10 +0300

    version with redux

---
+++ src/app/order-line-form/orderline-form.service.ts
@@ -0,0 +1,9 @@
+import {EventEmitter, Injectable} from '@angular/core';
+
+@Injectable()
+export class OrderlineFormService {
+  public orderlineFormState: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() { }
+
+}
