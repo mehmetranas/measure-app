5fee08309faf38284f3c4f5ebfdabf7dd653d5b3
mehmet <mhmtranas@gmail.com>
2018-03-11 12:40:11 +0300

    fixed orderline-final process compnenet

--- src/app/dialogs/order-final-process/order-final-process.component.ts
+++ src/app/dialogs/order-final-process/order-final-process.component.ts
@@ -1,10 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {orderStatus} from '../../helpers';
 
 @Component({
   selector: 'app-order-final-process',
-  template: './order-final-process.component.html',
+  templateUrl: './order-final-process.component.html',
   styles: [`
     .input-sm{
     width:70%
