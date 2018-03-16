c7435d551a96ace682120ee94a0aa6b982aa6433
mehmet <mhmtranas@gmail.com>
2018-03-16 10:36:48 +0300

    chaged package json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,96 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {NgRedux} from '@angular-redux/store';
-import {IAppState} from './redux/stores/app.store';
-import {ADD_ORDER_LINE} from './redux/redux.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  private mockOrderline = [{
-    order: {
-      totalAmount: 1500,
-      id: 23
-    },
-    product: {
-      productValue: 0,
-      patternCode: 'Desen1',
-      variantCode: 'Var1',
-      aliasName: 'Taç1'
-    },
-    locationName: 2,
-    locationType: 'Pencere 1',
-    piecesCount: 0,
-    mechanismStatus: 0,
-    productValue: 0,
-    propertyWidth: 500,
-    propertyHeight: 250,
-    mountType: 1,
-    sizeOfPile: 2,
-    lineDescription: 'Test2',
-    unitPrice: 3,
-    lineAmount: 500,
-    id: 56
-  },
-  {
-    order: {
-      totalAmount: 1500,
-      id: 23
-    },
-    product: {
-      productValue: 0,
-      patternCode: 'Desen3',
-      variantCode: 'Var3',
-      aliasName: 'Taç3'
-    },
-    locationName: 2,
-    locationType: 'Pencere 2',
-    piecesCount: 0,
-    mechanismStatus: 0,
-    productValue: 0,
-    propertyWidth: 500,
-    propertyHeight: 250,
-    mountType: 1,
-    sizeOfPile: 2,
-    lineDescription: 'Test',
-    unitPrice: 3,
-    lineAmount: 500,
-    id: 51
-  },
-  {
-    order: {
-      totalAmount: 1500,
-      id: 23
-    },
-    product: {
-      productValue: 0,
-      patternCode: 'Desen',
-      variantCode: 'Var',
-      aliasName: 'Taç'
-    },
-    locationName: 2,
-    locationType: 'Kapı 1',
-    piecesCount: 0,
-    mechanismStatus: 0,
-    productValue: 0,
-    propertyWidth: 500,
-    propertyHeight: 250,
-    mountType: 1,
-    sizeOfPile: 2,
-    lineDescription: 'Test',
-    unitPrice: 3,
-    lineAmount: 500,
-    id: 18
-  }];
+export class AppComponent{
 
-  constructor(private ngRedux: NgRedux<IAppState>){}
-
-  ngOnInit(): void {
-   // this.mockOrderline.forEach((orderline)=> {
-   //  this.ngRedux.dispatch({type:ADD_ORDER_LINE, orderline: orderline})
-   // })
-  }
-  title = 'app';
+  constructor(){}
 }
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {AuthService} from '../user/services/login.service';
 import 'rxjs/add/operator/map';
+import {state} from '@angular/animations';
 
 @Injectable()
-export class AuthGuardService implements CanActivate{
+export class AuthGuardService implements CanLoad{
+
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+  canLoad(route: Route):
     Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.checkSession()
       .map((isValid:boolean) => {
         if(isValid === true) return true;
 
-        this.authService.redirectUrl = state.url;
+        this.authService.redirectUrl = route.path;
         this.router.navigate(["/"]);
         return false;
     });
